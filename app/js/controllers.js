245e3b255c0f87e656d0e1db5042d769df095bf7
javpaw <javierbuitrago123@gmail.com>
2012-11-18 17:19:46 -0500

    modificacion controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@
 /* Controllers */
 function contratame($scope){
 
-  $scope.trabajos = ['plomero','electricista','carpintero','Maestro de Construccion','dfasdf','rtetret']
+  $scope.trabajos = ['plomero','electricista','carpintero','Maestro de Construccion']
 
   $scope.plomeros = [
     {
@@ -27,6 +27,56 @@ function contratame($scope){
     	'trabajos':['fasf','fff','fdf','afsdf'],
     	'foto':'/app/img/media/fotos/plomero-3.jpg'
     },
+
+
+    {
+        'nombre':'Pepito Perez',
+        'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-1.jpg'
+    },
+
+    {
+        'nombre':'Pepito Perez 2',
+        'descripcion':'Trabajo como plomero hace 150 anyos',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-2.jpg'
+    },
+
+    {
+        'nombre':'Pepito Perez 3',
+        'descripcion':'Trabajo como plomero hace 150 anyos',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-3.jpg'
+    },
+
+    {
+        'nombre':'Pepito Perez',
+        'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-1.jpg'
+    },
+
+    {
+        'nombre':'Pepito Perez 2',
+        'descripcion':'Trabajo como plomero hace 150 anyos',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-2.jpg'
+    },
+
+    {
+        'nombre':'Pepito Perez 3',
+        'descripcion':'Trabajo como plomero hace 150 anyos',
+        'trabajos':['fasf','fff','fdf','afsdf'],
+        'foto':'/app/img/media/fotos/plomero-3.jpg'
+    },
+
+
+
+
+
+
+
 
   ]
 
