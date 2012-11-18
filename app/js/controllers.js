dcb30d0499c7df13fb98622c34242d281259f122
javpaw <javierbuitrago123@gmail.com>
2012-11-18 15:53:28 -0500

    added files

---
+++ app/js/controllers.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+/* Controllers */
+function contratame($scope){
+
+  $scope.trabajos = ['plomero','electricista','carpintero','Maestro de Construccion','dfasdf','rtetret']
+
+  $scope.plomeros = [
+    {
+    	'nombre':'Pepito Perez',
+    	'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
+    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'foto':'/app/img/media/fotos/plomero-1.jpg'
+    },
+
+    {
+    	'nombre':'Pepito Perez 2',
+    	'descripcion':'Trabajo como plomero hace 150 anyos',
+    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'foto':'/app/img/media/fotos/plomero-2.jpg'
+    },
+
+    {
+    	'nombre':'Pepito Perez 3',
+    	'descripcion':'Trabajo como plomero hace 150 anyos',
+    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'foto':'/app/img/media/fotos/plomero-3.jpg'
+    },
+
+  ]
+
+}
+MyCtrl1.$inject = ['$scope'];
+
+
+
+
+/* Controllers */
+function MyCtrl1() {}
+MyCtrl1.$inject = [];
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
