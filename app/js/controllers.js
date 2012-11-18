5a7adb45c5d745c7682071af07debfc72bed9960
javpaw <javierbuitrago123@gmail.com>
2012-11-18 17:43:37 -0500

    hay vamos

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,26 +6,29 @@ function contratame($scope){
 
   $scope.trabajos = ['plomero','electricista','carpintero','Maestro de Construccion']
 
-  $scope.plomeros = [
+  $scope.personas = [
     {
     	'nombre':'Pepito Perez',
     	'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
     	'trabajos':['fasf','fff','fdf','afsdf'],
-    	'foto':'/app/img/media/fotos/plomero-1.jpg'
+    	'foto':'/app/img/media/fotos/plomero-1.jpg',
+        'trabajo':'plomero'
     },
 
     {
     	'nombre':'Pepito Perez 2',
     	'descripcion':'Trabajo como plomero hace 150 anyos',
     	'trabajos':['fasf','fff','fdf','afsdf'],
-    	'foto':'/app/img/media/fotos/plomero-2.jpg'
+    	'foto':'/app/img/media/fotos/plomero-2.jpg',
+        'trabajo':'Electricista'
     },
 
     {
     	'nombre':'Pepito Perez 3',
     	'descripcion':'Trabajo como plomero hace 150 anyos',
     	'trabajos':['fasf','fff','fdf','afsdf'],
-    	'foto':'/app/img/media/fotos/plomero-3.jpg'
+    	'foto':'/app/img/media/fotos/plomero-3.jpg',
+        'trabajo':'Carpintero'
     },
 
 
@@ -33,42 +36,8 @@ function contratame($scope){
         'nombre':'Pepito Perez',
         'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
         'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-1.jpg'
-    },
-
-    {
-        'nombre':'Pepito Perez 2',
-        'descripcion':'Trabajo como plomero hace 150 anyos',
-        'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-2.jpg'
-    },
-
-    {
-        'nombre':'Pepito Perez 3',
-        'descripcion':'Trabajo como plomero hace 150 anyos',
-        'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-3.jpg'
-    },
-
-    {
-        'nombre':'Pepito Perez',
-        'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
-        'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-1.jpg'
-    },
-
-    {
-        'nombre':'Pepito Perez 2',
-        'descripcion':'Trabajo como plomero hace 150 anyos',
-        'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-2.jpg'
-    },
-
-    {
-        'nombre':'Pepito Perez 3',
-        'descripcion':'Trabajo como plomero hace 150 anyos',
-        'trabajos':['fasf','fff','fdf','afsdf'],
-        'foto':'/app/img/media/fotos/plomero-3.jpg'
+        'foto':'/app/img/media/fotos/plomero-1.jpg',
+        'trabajo':'Maestro'
     },
 
 
