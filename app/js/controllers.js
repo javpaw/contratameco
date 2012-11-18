a1a97f17260ea76ce1fd146cd50ca8987361c7d3
fabioquimbay <fabio.quimbay@gmail.com>
2012-11-18 17:33:46 -0500

    Update app/js/controllers.js
    Actualizacion de algunas referencias y plomeros.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,23 +8,23 @@ function contratame($scope){
 
   $scope.plomeros = [
     {
-    	'nombre':'Pepito Perez',
-    	'descripcion':'Trabaje en wall street pero ahora me dedico a lo que mas me gusta: LA PLOMERIA!!!!!',
-    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'nombre':'Nelson Garcia',
+    	'descripcion':'He trabajado como plomero durante los ultimos 5 años, disfruto mi trabajo y poder ayudar a las personas a solucionar sus problemas en el hogar.',
+    	'trabajos':['Fundacion SantaFe','Calle 54 # 63 - 2','4328754','Contacto: Javier Romero'],
     	'foto':'/app/img/media/fotos/plomero-1.jpg'
     },
 
     {
-    	'nombre':'Pepito Perez 2',
-    	'descripcion':'Trabajo como plomero hace 150 anyos',
-    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'nombre':'Mauricio Cajamarca',
+    	'descripcion':'Trabajo como plomero ayudando a las personas que viven cerca a mi barrio en el centro de la Ciudad de Bogota',
+    	'trabajos':['Centro Don Bosco','Carrera 54 # 12 - 2','4561983','Contacto: Amanda Quimbay'],
     	'foto':'/app/img/media/fotos/plomero-2.jpg'
     },
 
     {
-    	'nombre':'Pepito Perez 3',
-    	'descripcion':'Trabajo como plomero hace 150 anyos',
-    	'trabajos':['fasf','fff','fdf','afsdf'],
+    	'nombre':'Bernardo Padilla',
+    	'descripcion':'Cuento con experiencia en Mexico, en pequeñas y medianas empresas, al igual que en instituciones educativas',
+    	'trabajos':['Fundacion Meyer','DF: Diagonal 23 # 12 -2','3857263','Contacto: Lizeth Ariza'],
     	'foto':'/app/img/media/fotos/plomero-3.jpg'
     },
 
