d92fafed994940ae670b2e2378bdb77594abd282
javpaw <javierbuitrago123@gmail.com>
2012-11-18 19:05:07 -0500

    mejor la pagina

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: contratame});
+    $routeProvider.when('/opcion2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/opcion1', {templateUrl: 'partials/partials3.html', controller: MyCtrl2});
+
+    $routeProvider.otherwise({redirectTo: '/opcion1'});
   }]);
